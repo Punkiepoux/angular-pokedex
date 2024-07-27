32cdb70767d1c593fcc6fdfb5c5440ea13000340
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-07-27 12:18:01 +0200

    rxjs

---
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from "@angular/core";
+import { Pokemon } from "../pokemon";
+import { Router } from "@angular/router";
+import { debounceTime, distinctUntilChanged, Observable, Subject, switchMap } from "rxjs";
+import { PokemonService } from "../pokemon.service";
+
+@Component({
+  selector: "app-search-pokemon",
+  templateUrl: "./search-pokemon.component.html",
+})
+export class SearchPokemonComponent implements OnInit {
+  searchTerms = new Subject<string>();
+  pokemons$: Observable<Pokemon[]>;
+
+  constructor(
+    private router: Router, 
+    private pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    this.pokemons$ = this.searchTerms.pipe(
+      debounceTime(300),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.pokemonService.searchPokemonList(term))
+    );
+  }
+
+  search(term: string) {
+    this.searchTerms.next(term);
+  }
+
+  goToDetail(pokemon: Pokemon) {
+    const link = ["/pokemon", pokemon.id];
+    this.router.navigate(link);
+  }
+}
+function SwitchMap(arg0: (term: string) => Observable<Pokemon[]>): import("rxjs").OperatorFunction<string, Pokemon[]> {
+  throw new Error("Function not implemented.");
+}
+
